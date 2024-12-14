52b3b0600364714ad580e1fd8ca0cdf2b8fa569c
Pheaktra <130367888+pheaktraa@users.noreply.github.com>
2024-12-14 17:15:39 +0700

    my second amit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'custom-pattern': url("https://i.gifer.com/J4o.gif"),
+      },
       colors: {
         primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
-      }
+      },
+      
     },
   },
   plugins: [],
